377f80dc81ea909378dfcb81a7301a066462738f
marijn <marijn@marijns-MacBook-Pro.local>
2017-02-25 14:25:36 -0600

    1969201 bug fix?

--- drat-trim.c
+++ drat-trim.c
@@ -292,7 +292,7 @@ void printProof (struct solver *S) {
       if ((ad & 1) == 0) {
         if (lastAdded == 0) fprintf (S->lratFile, "0\n");
         lastAdded = lemmas[ID] >> 1; }
-//      else if (!lemmas[1] && (ad & 1)) continue; // don't delete unit clauses
+      else if (!lemmas[1] && (ad & 1)) continue; // don't delete unit clauses
       else if (ad & 1) {
         if (lastAdded != 0) fprintf (S->lratFile, "%i d ", lastAdded);
         lastAdded = 0;
