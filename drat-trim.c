7dc58bae71883a490814a22acdf6df00bced4225
Marienus Heule <mheule@SCS003B8.lan>
2021-02-20 10:01:27 -0500

    warning

--- drat-trim.c
+++ drat-trim.c
@@ -787,7 +787,10 @@ int verify (struct solver *S, int begin, int end) {
     if (d && lemmas[1]) { // if delete and not unit
       if ((S->reason[abs (lemmas[0])] - 1) == (lemmas - S->DB)) { // what is this check?
         if (S->mode != FORWARD_SAT) { // ignore pseudo unit clause deletion
-          if (S->verb) { printf ("c ignoring deletion instruction %li: ", (lemmas - S->DB)); printClause (lemmas); }
+          if (S->warning != NOWARNING) {
+            printf ("\rc WARNING: ignoring deletion instruction %li: ", (lemmas - S->DB)); printClause (lemmas); }
+          if (S->warning == HARDWARNING) exit (HARDWARNING);
+//          if (S->verb) { printf ("c ignoring deletion instruction %li: ", (lemmas - S->DB)); printClause (lemmas); }
 //        if (S->mode == BACKWARD_UNSAT) { // ignore pseudo unit clause deletion
           S->proof[step] = 0; }
         else { // if (S->mode == FORWARD_SAT) { // also for FORWARD_UNSAT?
