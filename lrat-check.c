15726b146cb25d7965e87c4b166bca9c2ff2544f
Marienus Heule <mheule@SCS003B8.SP.CS.CMU.EDU>
2021-09-12 18:33:11 -0400

    date update

--- lrat-check.c
+++ lrat-check.c
@@ -1,6 +1,6 @@
 /************************************************************************************[lrat-check.c]
-Copyright (c) 2017-2020 Marijn Heule, Carnegie Mellon University
-Last edit: September 5, 2020
+Copyright (c) 2017-2021 Marijn Heule, Carnegie Mellon University
+Last edit: September 12, 2021
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
